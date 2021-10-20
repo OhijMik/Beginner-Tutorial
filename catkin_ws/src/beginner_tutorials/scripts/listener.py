e1b36f66712ffbc5f45d3f3ca8ecd721d49fa5e3
OhijMik <1kimjih2@hdsb.ca>
2021-10-20 10:35:53 -0400

    Commanding the Turtle

--- catkin_ws/src/beginner_tutorials/scripts/listener.py
+++ catkin_ws/src/beginner_tutorials/scripts/listener.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Twist
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -14,7 +14,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("/turtle1/command_velocity", Twist, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- catkin_ws/src/beginner_tutorials/scripts/talker.py
+++ catkin_ws/src/beginner_tutorials/scripts/talker.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('/turtle1/command_velocity', Twist, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = self.__slots__ = '[2.0, 0.0, 0.0]' '[0.0, 0.0, 1.8]'
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
