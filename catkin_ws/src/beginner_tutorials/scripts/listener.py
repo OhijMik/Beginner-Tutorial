17d0e0a33506c19880f8821e229f494264836141
OhijMik <1kimjih2@hdsb.ca>
2021-10-22 13:42:50 -0400

    Moving the turtle

--- catkin_ws/src/beginner_tutorials/scripts/listener.py
+++ catkin_ws/src/beginner_tutorials/scripts/listener.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import rospy
-from std_msgs.msg import Twist
+from std_msgs.msg import String
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -14,7 +14,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("/turtle1/command_velocity", Twist, callback)
+    rospy.Subscriber("chatter", String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- catkin_ws/src/beginner_tutorials/scripts/talker.py
+++ catkin_ws/src/beginner_tutorials/scripts/talker.py
@@ -2,19 +2,58 @@
 # license removed for brevity
 import rospy
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
 
-def talker():
-    pub = rospy.Publisher('/turtle1/command_velocity', Twist, queue_size=10)
+
+wantedPosX = 3.0    # The coordinates of where you want the turtle to be at
+wantedPosY = 3.0
+
+currentPosX = 5.54
+currentPosY = 5.54
+
+
+def cmd_vel_talker():
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = self.__slots__ = '[2.0, 0.0, 0.0]' '[0.0, 0.0, 1.8]'
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        cmd_vel = Twist()
+        cmd_vel.linear.x = posX
+        cmd_vel.linear.y = posY
+        cmd_vel.angular.z = 0.0
+        rospy.loginfo(cmd_vel)
+        pub.publish(cmd_vel)
         rate.sleep()
+        break
+
+
+def pose_talker():
+    pub = rospy.Publisher('/turtlesim/Pose', Vector3, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz    
+    while not rospy.is_shutdown():
+        position = Vector3()
+        position.x = currentPosX
+        position.y = currentPosY
+        position.z = 0.0
+        rospy.loginfo(position)
+        pub.publish(position)
+        rate.sleep()
+        break
+
 
 if __name__ == '__main__':
     try:
-        talker()
+        
+        posX = wantedPosX - currentPosX    
+        posY = wantedPosY - currentPosY
+        cmd_vel_talker()
+        pose_talker()
+        currentPosX = currentPosX + posX
+        currentPosY = currentPosY + posY
+        cmd_vel_talker()
+        pose_talker()
+
     except rospy.ROSInterruptException:
         pass
+
