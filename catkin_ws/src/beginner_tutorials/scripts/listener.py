d4b2ce6988087dfccca650cb328330156f2e92cc
OhijMik <1kimjih2@hdsb.ca>
2021-12-03 09:41:33 -0500

    Beginner_tutorials update

--- catkin_ws/src/beginner_tutorials/scripts/listener.py
+++ catkin_ws/src/beginner_tutorials/scripts/listener.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
+
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
-def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+def pose_listener():
+    while not rospy.is_shutdown():  # While loop while rospy is not shutdown
+        rospy.init_node('listener', anonymous=True) # Initializing the listener code
+        rospy.Subscriber("/turtle1/pose", Pose, turtle_callback)    # Making a subscriber that subscribes to turtle1/pose
+        rospy.Subscriber("/turtlesim/Pose", Vector3, callback)  # Making a subscriber that subscribes to turtlesim/Pose
+        global pub
+        pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)     # Publishing to turtle1/cmd_vel
+        rospy.spin()    # Keeps the code from exiting until this node is stopped
 
-    rospy.Subscriber("chatter", String, callback)
+def turtle_callback(data):  # Recieving the data of where the turtle is and making the variable global
+    pose = data
+    global currentPosX  # Making the variable global
+    global currentPosY
+    currentPosX = pose.x
+    currentPosY = pose.y
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
+def callback(position): # Showing where I want the turtle to be at and running cmd_listener
+    rate = rospy.Rate(5)    # The rate the loop goes at
+    rospy.loginfo(position) # Showing the position
+    errorX = position.x - currentPosX    # The calculation
+    errorY = position.y - currentPosY
+    cmd_vel = Twist()           # Declaring the variable is a Twist message type
+    cmd_vel.linear.x = errorX   # Moving the turtle
+    cmd_vel.linear.y = errorY
+    pub.publish(cmd_vel)    # Publishing the velocity to turtle1/cmd_vel using Twist
+    rate.sleep()
+        
+        
 if __name__ == '__main__':
-    listener()
+    pose_listener()
--- catkin_ws/src/beginner_tutorials/scripts/talker.py
+++ catkin_ws/src/beginner_tutorials/scripts/talker.py
@@ -1,59 +1,24 @@
 #!/usr/bin/python3
 # license removed for brevity
 import rospy
-from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 
-wantedPosX = 3.0    # The coordinates of where you want the turtle to be at
-wantedPosY = 3.0
-
-currentPosX = 5.54
-currentPosY = 5.54
-
-
-def cmd_vel_talker():
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        cmd_vel = Twist()
-        cmd_vel.linear.x = posX
-        cmd_vel.linear.y = posY
-        cmd_vel.angular.z = 0.0
-        rospy.loginfo(cmd_vel)
-        pub.publish(cmd_vel)
-        rate.sleep()
-        break
-
-
 def pose_talker():
-    pub = rospy.Publisher('/turtlesim/Pose', Vector3, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz    
-    while not rospy.is_shutdown():
-        position = Vector3()
-        position.x = currentPosX
-        position.y = currentPosY
-        position.z = 0.0
-        rospy.loginfo(position)
-        pub.publish(position)
-        rate.sleep()
-        break
-
+    pub = rospy.Publisher('/turtlesim/Pose', Vector3, queue_size=10)    # Publishing to turtlesim/Pose
+    rospy.init_node('talker', anonymous=True)   # Initializing this code
+    rate = rospy.Rate(10)
+    position = Vector3()
+    position.x = 9
+    position.y = 9
+    rospy.loginfo(position)
+    pub.publish(position)
+    rate.sleep()
+        
 
 if __name__ == '__main__':
-    try:
-        
-        posX = wantedPosX - currentPosX    
-        posY = wantedPosY - currentPosY
-        cmd_vel_talker()
+    try: 
         pose_talker()
-        currentPosX = currentPosX + posX
-        currentPosY = currentPosY + posY
-        cmd_vel_talker()
-        pose_talker()
-
+            
     except rospy.ROSInterruptException:
         pass
-
